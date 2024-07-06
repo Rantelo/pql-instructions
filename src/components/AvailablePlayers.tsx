6917c5bcbf6dd52839f1f372506160b5a4d4423c
Roberto Antelo <roberto_antelo@hotmail.com>
2024-07-06 16:54:51 -0600

    Remove player from outline

--- src/components/AvailablePlayers.tsx
+++ src/components/AvailablePlayers.tsx
@@ -10,6 +10,25 @@ const AvailablePlayers = () => {
       .catch(error => console.error(error));
   }, []);
 
+  const specialAbility = position => {
+    switch (position) {
+      case "Chaser":
+        return "Light Speed"
+      case "Seeker":
+        return "Enhanced Vision"
+      case "Beater":
+        return "Power Swing"
+      case "Keeper":
+      default:
+        return "Bold Reflexes"
+    }
+  }
+
+  const removeFromList = id => {
+    const filteredData = data.filter(element => element.id !== id);
+    setData(filteredData);
+  }
+
   return (
     <>
       <div className="border-b border-gray-900/10 pb-12"></div>
@@ -32,8 +51,8 @@ const AvailablePlayers = () => {
                   <td className="px-6 py-4">{element.name}</td>
                   <td className="px-6 py-4">{element.age}</td>
                   <td className="px-6 py-4">{element.position}</td>
-                  <td className="px-6 py-4">{element.name}</td>
-                  <td className="px-6 py-4"></td>
+                  <td className="px-6 py-4">{specialAbility(element.position)}</td>
+                  <td className="px-6 py-4">{<RemoveButton removeItem={() => removeFromList(element.id)} />}</td>
                 </tr>
               )
             })
@@ -41,6 +60,20 @@ const AvailablePlayers = () => {
         </tbody>
       </table>
     </>
+  )
+}
+
+const RemoveButton = ({removeItem}) => {
+  return (
+    <button
+      type="button"
+      className="text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none  font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 "
+      onClick={removeItem}
+      >
+      <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+      </svg>
+    </button>
   )
 }
 
--- src/components/CreateTeam.tsx
+++ src/components/CreateTeam.tsx
@@ -51,7 +51,7 @@ const CreateTeam = () => {
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     {...register('teamName')}
                   />
-                  {errors.teamName && <span>{errors.teamName.message}</span>}
+                  {errors.teamName && <span className="text-red-400 text-xs">{errors.teamName.message}</span>}
                 </div>
               </div>
             </div>
