5143d910167c921fe82d38e8718a0b41909584fc
Roberto Antelo <roberto_antelo@hotmail.com>
2024-07-05 22:33:06 -0600

    Create teams form

---
+++ src/components/CreateTeam.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { z, ZodType } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+
+type FormData = {
+  teamName: string;
+  description: string;
+};
+
+const CreateTeam = () => {
+
+  const schema: ZodType<FormData> = z.object({
+    teamName: z.string({
+      required_error: 'Team name required'
+    })
+    .min(1),
+    description: z.string().max(100)
+  });
+
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors } 
+  } = useForm<FormData>({ resolver: zodResolver(schema) })
+
+  const submitNewTeam = (data: FormData) => {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(submitNewTeam)}>
+        <label>Team Name: </label>
+        <input type="text" { ...register('teamName') } />
+        {errors.teamName && <span>{errors.teamName.message}</span>}
+        <label>Description: </label>
+        <input type="text" { ...register('description') } />
+        {errors.description && <span>{errors.description.message}</span>}
+
+        <input type="submit" />
+      </form>
+    </>
+  )
+}
+
+export default CreateTeam;
